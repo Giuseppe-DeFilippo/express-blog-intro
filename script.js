28d562a3fc3cde09f52b1d73a73022348caafbbd
giuse <giuseppedefilippo03@gmail.com>
2024-12-02 15:27:26 +0100

    adding public and img at array

--- script.js
+++ script.js
@@ -12,6 +12,47 @@ const PORT = 3000
 app.get("/", (req, res) => {
     res.send("server del mio blog")
 })
+
+app.use(express.static("public"));
+
+
+const lista = [
+    post1 = {
+        img: "ciambellone.jpeg",
+        titolo: "ciambellone",
+        contenuto: "ciambella",
+        tags: ["ciambellone", "ciambella"]
+    },
+    post2 = {
+        img: "cracker_barbabietola.jpeg",
+        titolo: "cracker barbabietola",
+        contenuto: "crecs",
+        tags: ["cracker", "crecs"]
+    },
+    post3 = {
+        img: "pane_fritto.jpeg",
+        titolo: "pane fritto dolce",
+        contenuto: "pane ",
+        tags: ["pane", "fritto"]
+    },
+    post4 = {
+        img: "pasta_barbabietola.jpeg",
+        titolo: "pasta barbabietola",
+        contenuto: "barbabietola",
+        tags: ["pasta", "barbabietola"]
+    },
+    post5 = {
+        img: "torta_paesana.jpeg",
+        titolo: "torta paesana",
+        contenuto: "torta",
+        tags: ["torta", "paesana"]
+    }
+];
+app.get("/bacheca", (req, res) => {
+    res.json(lista)
+})
+
+
 app.listen(PORT, () => {
     console.log(`Server in esecuzione su http://localhost:${PORT}`);
 });
