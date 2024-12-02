6eb1225e27fe05464d29e2c0225c57f162f02634
giuse <giuseppedefilippo03@gmail.com>
2024-12-02 14:28:59 +0100

    start

---
+++ script.js
@@ -0,0 +1,14 @@
+// - Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
+// - Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags(tags è un array di stringhe)
+//     - Creiamo poi una rotta / bacheca che restituisca un oggetto json con la lista dei post e il conteggio, partendo da un array.
+// - Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+// - Testare su postman
+
+
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+    res.send("server del mio blog")
+})
